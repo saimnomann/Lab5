ac0eaa915ebcce9f47c1f01d8ed305ffb05dc854
saimnomann <141673697+saimnomann@users.noreply.github.com>
2024-09-27 19:21:37 +0500

    Delete q6.c

--- q6.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main(){
-	int num,ld;
-	printf("Please enter the number");
-	scanf("%d",&num);
-	ld=num%10;
-	ld == 0?printf("Last Digit is zero"):printf("Last Digit is non zero");
-}
